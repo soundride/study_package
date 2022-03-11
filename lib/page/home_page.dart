00e4d9858adacac549be855c15b841bfa49bb726
soundride <kr.soundride@gmail.com>
2022-03-11 11:18:28 +0900

    알림발송

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:study_package/main.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage ({ Key? key }) : super(key: key);
@@ -31,9 +35,40 @@ class _HomepageState extends State<Homepage> {
             }, 
             child: const Text('btn'),
           ),
+          ElevatedButton(
+            onPressed: () async {
+              final notification = FlutterLocalNotificationsPlugin();
+
+              const android = AndroidNotificationDetails(
+              '0',
+              '알림테스트',
+              channelDescription: '알림 테스트 바디 부분',
+              importance: Importance.max,
+              priority: Priority.max,
+              );
+              const ios = IOSNotificationDetails();
+              const detail = NotificationDetails(
+                android: android,
+                iOS: ios,
+              );
+
+              final permission = Platform.isAndroid
+                    ? true
+                    : await notification
+                            .resolvePlatformSpecificImplementation<
+                              IOSFlutterLocalNotificationsPlugin>()
+                          ?.requestPermissions(alert: true, badge: true, sound: true) ??
+                        false;
+                        print('permission ');
+            }, 
+            child: const Text('add alarm'),
+          ),
           const Center(child: Text('hi')),
         ],
       )
     );
   }
+}
+
+class _showToast {
 }
